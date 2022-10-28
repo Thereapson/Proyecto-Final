008c99255637b5f412e05d702e3be29e8a8edabb
Andres <andrefe1808@gmail.com>
2022-10-28 13:57:16 -0500

    Push de componente y ruta

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/navbar/navbar';
 import Products from './components/products/products';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DetailCard from './components/detail/detail';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
+          <Route path="/detail/:id" element={<DetailCard/>}/>
           <Route path="/products" element={<Products />} />
           <Route path="/products?search=:search" element={<Products />} />
         </Routes>
