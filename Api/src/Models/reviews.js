403b8224b3ab79137ee9efdb68fce6ba567184f7
Makami-Yv <attercoppe@protonmail.com>
2022-11-01 14:24:38 -0600

    Nuevos modelos de la DB

---
+++ Api/src/Models/reviews.js
@@ -0,0 +1,31 @@
+// MODELO REVIEWS
+const { Schema, model } = require('mongoose')
+
+// Schema del modelo
+const reviewSchema = new Schema({
+    user_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    product_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Product'
+    },
+    score: {
+        type: Number
+    },
+    review: {
+        type: String
+    },
+    date: {
+        type: Date
+    }
+}, 
+{
+    timestamps: true,
+    versionKey: false,
+})
+
+const reviewModel = model("Review", reviewSchema);
+
+module.exports = reviewModel;
