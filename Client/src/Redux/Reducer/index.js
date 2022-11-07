bd4ef79a7ae1d94b20862536a09a4187f3e87f1f
Makami-Yv <attercoppe@protonmail.com>
2022-11-07 14:41:46 -0600

    cambios en reducer para carrito

--- Client/src/Redux/Reducer/index.js
+++ Client/src/Redux/Reducer/index.js
@@ -4,9 +4,10 @@ import {
     GET_PRODUCT_BY_ID,
     GET_PRODUCTS_BY_SEARCH,
     GET_CATEGORIES,
+    ADD_PRODUCT,
     CLEAN_DETAILS,
     GET_PRODUCTS_BY_MIN_MAX,
-    GET_USER
+    GET_USER,
     GET_CART,
     REMOVE_FROM_CART,
     CLEAN_PRODUCTS,
@@ -71,6 +72,42 @@ const rootReducer = (state = initialState, action) => {
                 ...state,
                 categories: action.payload,
             };
+
+        case REMOVE_FROM_CART:
+            let productToRemove = action.payload;
+            let productInCartToRemove = state.cart.find((product) => product.id === productToRemove.id);
+            if (productInCartToRemove.quantity > 1) {
+                return {
+                    ...state,
+                    cart: state.cart.map((product) => (product.id === productToRemove.id ? { ...product, quantity: product.quantity - 1 } : product)),
+                };
+            } else {
+                return {
+                    ...state,
+                    cart: state.cart.filter((product) => product.id !== productToRemove.id),
+                };
+            }
+            
+        case ADD_PRODUCT:
+            // from action
+            // return {
+            //     type: ADD_PRODUCT,
+            //     payload: products,
+            // };
+            let productToAdd = action.payload;
+            console.log("productToAdd: ", productToAdd);
+            let productInCart = state.cart.find((product) => product.id === productToAdd.id);
+            if (productInCart) {
+                return {
+                    ...state,
+                    cart: state.cart.map((product) => (product.id === productToAdd.id ? { ...product, quantity: product.quantity + 1 } : product)),
+                };
+            } else {
+                return {
+                    ...state,
+                    cart: [...state.cart, { ...productToAdd, quantity: 1 }],
+                };
+            }
 
         case CLEAN_DETAILS:
             return {
