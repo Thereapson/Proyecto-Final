1cd43a5d5c8f6ecb484c50e366d0c928fb15e875
Rodrigo2297 <rodrigomadona22@gmail.com>
2022-11-15 15:46:37 -0300

    login-y-landing-prueba

--- Client/src/components/Register/userDetail.jsx
+++ Client/src/components/Register/userDetail.jsx
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import Navbar from '../navbar/navbar';
 
-export default class UserDetail extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            userData: ""
-        }
-    }
-    componentDidMount() {
+const userDetail = () => {
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //         userData: ""
+    //     }
+    // }
+
+    const userData = useSelector(state => state.userData)
+
+    const [user, setUser] = useState("")
+
+    useEffect(() => {
         // const token = this.state
         fetch('/users/userData', {
             method: 'POST',
@@ -25,34 +32,36 @@ export default class UserDetail extends Component {
             .then((res) => res.json())
             .then((data) => {
                 console.log(data, 'userData')
-                this.setState({ userData: data.data })
+                setUser({ userData: data.data })
             })
-    }
-    render() {
-        return (
-            <div>
-                <Navbar />
-                <div class="h-screen bg-gray-200  dark:bg-gray-800   flex flex-wrap items-center  justify-center  ">
-                    <div class="container lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
-                        <div class=" h-32 overflow-hidden" >
-                            <img class="w-full" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80" alt="" />
-                        </div>
-                        <div class="flex justify-center px-5  -mt-12">
-                            <img class="h-32 w-32 bg-white p-2 rounded-full   " src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80" alt="" />
+    }, [])
 
-                        </div>
-                        <div class=" ">
-                            <div class="text-center px-14">
-                                <h2 class="text-gray-800 text-3xl font-bold">Hola! {this.state.userData.full_name}</h2>
-                                <a class="text-gray-400 mt-2 hover:text-blue-500" target="BLANK()">{this.state.userData.email}</a>
-                                <p class="mt-2 text-gray-500 text-sm">Bienvenido a compuDevs </p>
-                            </div>
-                            <hr class="mt-6" />
+    return (
+        <div>
+            <Navbar />
+            <div class="h-screen bg-gray-200  dark:bg-gray-800   flex flex-wrap items-center  justify-center  ">
+                <div class="container lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
+                    <div class=" h-32 overflow-hidden" >
+                        <img class="w-full" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80" alt="" />
+                    </div>
+                    <div class="flex justify-center px-5  -mt-12">
+                        <img class="h-32 w-32 bg-white p-2 rounded-full   " src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80" alt="" />
 
+                    </div>
+                    <div class=" ">
+                        <div class="text-center px-14">
+                            <h2 class="text-gray-800 text-3xl font-bold">Hola! {userData.full_name}</h2>
+                            <a class="text-gray-400 mt-2 hover:text-blue-500" target="BLANK()">{userData.email}</a>
+                            <p class="mt-2 text-gray-500 text-sm">Bienvenido a compuDevs </p>
                         </div>
+                        <hr class="mt-6" />
+
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+
 }
+
+export default userDetail
